ed4f05878970895b45e3885587432605f01c4e5e
mach-911 <enidev911@gmail.com>
2023-05-22 06:28:04 +0000

    uso icorrecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
+const Producto = (props) => {
+  return (
+    // Aquí no debería ir la key
+    <li key={props.cod.toString()}>{props.nombre}</li>
+  );
+};
+
 const App = () => {
   const productos = [
-    {cod: 1001, nombre: "Producto-1"}, 
-    {cod: 1002, nombre: "Producto-2"},
-    {cod: 1003, nombre: "Producto-3"}
-	];
-  const listaProductos = productos.map((producto) => {
-			return (
-        <li key={producto.cod.toString()}>
-          { producto.nombre }
-        </li>
-      )
-	});
-
-  return (
-    <ul>{ listaProductos }</ul>
-  )
-}
+    { cod: 1001, nombre: "Producto-1" },
+    { cod: 1002, nombre: "Producto-2" },
+    { cod: 1003, nombre: "Producto-3" },
+  ];
+  const listaDeProductos = productos.map((producto) => (
+    // Aquí debería ir la key
+    <Producto nombre={producto.nombre} cod={producto.cod} />
+  ));
+  return <ul>{listaDeProductos}</ul>;
+};
 export default App;
-
